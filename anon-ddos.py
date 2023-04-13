07ce0389a4ab216cc9b765607150085340237b0a
ANONYMOUS | MOJO <130356338+Anonymousnism@users.noreply.github.com>
2023-04-14 02:11:22 +0700

    Anonymous

--- anon-ddos.py
+++ anon-ddos.py
@@ -21,7 +21,7 @@ print "           $$$$$$$$$$$           $$$$$$$$$$$    $$$     $$$      $    $  
 print "           $$$$$$$$$$$           $$$$$$$$$$$    $$$$$$$$$$$   $  $    $   $    "
 print "           $$$$$$$$$$$           $$$$$$$$$$$    $$$$$$$$$$$    $$$    $$$$$    "
 print 
-print "["+B+""+R+"#"+N+"] "+B+""+R+"Author : White Eagle"+N+"   Eagle Dos From - "+B+""+R+"Zoom Hackers"+N
+print "["+B+""+R+"#"+N+"] "+B+""+R+"Author : Mojo Anonymous"+N+"   Eagle Dos From - "+B+""+R+"Zoom Hackers"+N
 print
 print("\033[32m================================================================\033[0m")
 print("\033[32mTool devoloped : white-eagle\033[0m")
